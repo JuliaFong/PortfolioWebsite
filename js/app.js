79d2f455a5186fffce898c1e558621ed5b258b54
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2020-01-06 18:09:05 -0800

    adds JS

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    const $body = $('body');
+    const $navbar = $('.navbar-default');
+    const $offsetY = $navbar.offset().top + 10;
+    const $menuButton = $('button.navbar-toggle');
+    const $menuIcon = $('.navbar-toggle.glyphicon');
+    const $collapsedMenuItem = $('.navbar-collapse.collapse li')
+    const $scrollButton = $('.scroll')
+    const $socialIcon = $('.social');
+})
