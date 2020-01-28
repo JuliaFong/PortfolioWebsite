77b4bd73c05b7cb1a041be1954b1a97e2923ae16
Julia Fong <Juliamzfong@gmail.com>
2020-01-27 17:42:35 -0800

    Add portfolio information

--- js/app.js
+++ js/app.js
@@ -7,17 +7,9 @@ $(document).ready(function () {
     const $collapsedMenuItem = $('.navbar-collapse.collapse li');
     const $scrollButton = $('.scroll');
     const $socialIcon = $('.social');
-    const $contact = $('.scroll')
+    const $contact = $('.scroll');
 })
 
-function scroll () {
-    if($(window).scrollTop() <= ($offsetY) {
-        $navbar.addClass('menu-fixed').css('background-color', 'rgba(255, 254, 0.99') 
-    } else {
-        $navbar.addClass('menu-fixed').css('background-color', 'transparent')
-    }
-}
-document.onscroll = scroll
 
 function openMenu() {
     $menuIcon.removeClass('glyphicon-menu-hamburger').addClass('glyphicon-remove active')
@@ -26,17 +18,31 @@ function openMenu() {
     if(!$navbar.hasClass('menu-fixed')){
         $navbar.css('background-color', rgba(255, 253, 0.99))
     }
-    $modalBackDropDiv.on('click,'function' () {
+    $modalBackDropDiv.on('click', function () {
         $('.navbar-toggle').click()
         closeMenu()
     })
 }
 function closeMenu(){
     $menuIcon.removeClass('glyphicon-remove active').addClass('glyphicon-menu-hamburger')
-        $modalBackDropDiv.css('z-index', 1025).remove() 
-        if(!$navbar.hasClass('menu-fixed'))
-        {
-            $navbar.css('background-color', 'transparent')
-        }
+    $modalBackDropDiv.css('z-index', 1025).remove() 
+    if(!$navbar.hasClass('menu-fixed'))
+    {
+        $navbar.css('background-color', 'transparent')
+    }
 }
+
+function scroll () {
+    if ($(window).scrollTop() >=
+    $offsetY) {
+        $navbar.addClass('menu-fixed')
+    } else {
+        $navbar.removeClass('menu-fixed').css('background-color',
+        'transparent')
+    }
+}
+document.onscroll = scroll;
+
+
+
 
